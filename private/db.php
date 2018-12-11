9f7840fc9f4b0dff92091234ed11db646a8af2eb
DESKTOP-KCE3Q6L\Gianluca <gianluca.cecchi@cynny.com>
2018-12-12 00:01:18 +0100

    fixxed db commit/rollback

--- private/db.php
+++ private/db.php
@@ -11,10 +11,12 @@ class DatabaseConnection {
      */
     public function __construct() {
         $config = include('config.php');
-        $this->conn = mysqli_connect($config['servername'], $config['username'], $config['password'], $config['db']);
+        $this->conn = new mysqli($config['servername'], $config['username'], $config['password'], $config['db']);
         if (!$this->conn) {
             die("Connection failed: " . mysqli_connect_error());
         }
+
+        $this->conn->autocommit(FALSE);
     }
 
     public function __destruct() {
@@ -29,25 +31,25 @@ class DatabaseConnection {
         if (!$this->transaction_started) {
             $this->transaction_success = true;
             $this->transaction_started = true;
-            $this->conn->query("START TRANSACTION");
+            $this->conn->begin_transaction(MYSQLI_TRANS_START_READ_WRITE);
         }
     }
 
     private function update_transaction_status($res)
     {
         if ($this->transaction_started) {
-            $this->transaction_success = $res && $this->transaction_started;
+            $this->transaction_success = $res && $this->transaction_started && $this->transaction_success;
         }
     }
 
     public function commit_transaction() {
         if ($this->transaction_started) {
             $this->transaction_started = false;
-            if($this->transaction_success){
-                $this->conn->query("COMMIT");
+            if($this->transaction_success) {
+                $this->conn->commit();
                 return true;
             } else {
-                $this->conn->query("ROLLBACK");
+                $this->conn->rollback();
                 return false;
             }
         }
@@ -56,15 +58,15 @@ class DatabaseConnection {
 
     public function rollback() {
         $this->transaction_started = false;
-        $this->conn->query("ROLLBACK");
+        $this->conn->rollback();
     }
 
     public function query($q) {
         $res = $this->conn->query($q);
         $this->update_transaction_status($res);
-        if(!$this->transaction_success) {
+        /*if(!$this->transaction_success) {
             echo $this->transaction_success." FAILED: ".$q."\n";
-        }
+        }*/
         return  $res;
     }
 
